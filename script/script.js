5e57a05496158dbd011d11515512fea2bddfb674
CAHbEJIb <cahbejib@gmail.com>
2022-06-22 22:15:18 +0300

    готово

--- script/script.js
+++ script/script.js
@@ -1,4 +1,40 @@
 const container = document.querySelector('.cat-card');
+
+
+const containeractiveElNow = document.querySelectorAll('.cat-card__container--active');
+
+
+
+
+// console.log(containeractiveElNow[0]);
+// containeractiveElNow.forEach((elem) => {
+//     elem.addEventListener('mouseover', removeHover, false)
+
+//     function removeHover() {
+//         console.log(event.target);
+//         console.log('event.srcElement', event.srcElement);
+//         elem.classList.add('cat-card__container--active-hover');
+//         elem.removeEventListener('mouseover', removeHover, false);
+
+
+//         // if (!event.target.closest('.cat-card__container--active'));
+//         // {
+
+//         // }
+//         // elem.classList.remove('cat-card__container--active-hover');
+
+//     }
+
+
+
+
+// });
+
+
+
+
+
+
 container.addEventListener('click', () => {
     const el = event.target;
     const isBlock = el.closest('.cat-card__container');
@@ -7,24 +43,33 @@ container.addEventListener('click', () => {
         isBlock.classList.toggle('cat-card__container--active');
         let containerActive = container.querySelector('.cat-card__container--active');
 
-        console.log('тут');
+
+
         if (isBlock.classList.contains('cat-card__container--active')) {
-            console.log('зашли');
             isBlock.addEventListener('mouseenter', switchOnHover, false);
         } else {
-            console.log('оту');
             isBlock.classList.remove('cat-card__container--active-hover');
             isBlock.removeEventListener('mouseenter', switchOnHover, false);
         }
 
         function switchOnHover() {
-            console.log('зашли2');
             isBlock.classList.add('cat-card__container--active-hover');
-            isBlock.removeEventListener('mouseenter', switchOnHover, false); console.log('ушли');
+            isBlock.removeEventListener('mouseenter', switchOnHover, false);
         }
         isBlock.classList.remove('cat-card__container--active-hover');
     }
 });
+
+container.addEventListener('mousedown', disableCard, false);
+function disableCard() {
+    if (event.which === 2) {
+        const el = event.target;
+        const isBlock = el.closest('.cat-card__container');
+
+        if (isBlock) { isBlock.classList.toggle('cat-card__container--disable'); }
+    }
+
+}
 
 
 
