192c3690fa2683c2a88845ce3a3d95be55bcefc6
CAHbEJIb <cahbejib@gmail.com>
2022-06-22 22:25:47 +0300

    вычещен код

--- script/script.js
+++ script/script.js
@@ -1,40 +1,5 @@
 const container = document.querySelector('.cat-card');
 
-
-const containeractiveElNow = document.querySelectorAll('.cat-card__container--active');
-
-
-
-
-// console.log(containeractiveElNow[0]);
-// containeractiveElNow.forEach((elem) => {
-//     elem.addEventListener('mouseover', removeHover, false)
-
-//     function removeHover() {
-//         console.log(event.target);
-//         console.log('event.srcElement', event.srcElement);
-//         elem.classList.add('cat-card__container--active-hover');
-//         elem.removeEventListener('mouseover', removeHover, false);
-
-
-//         // if (!event.target.closest('.cat-card__container--active'));
-//         // {
-
-//         // }
-//         // elem.classList.remove('cat-card__container--active-hover');
-
-//     }
-
-
-
-
-// });
-
-
-
-
-
-
 container.addEventListener('click', () => {
     const el = event.target;
     const isBlock = el.closest('.cat-card__container');
@@ -42,9 +7,6 @@ container.addEventListener('click', () => {
     if (isBlock) {
         isBlock.classList.toggle('cat-card__container--active');
         let containerActive = container.querySelector('.cat-card__container--active');
-
-
-
         if (isBlock.classList.contains('cat-card__container--active')) {
             isBlock.addEventListener('mouseenter', switchOnHover, false);
         } else {
@@ -65,10 +27,8 @@ function disableCard() {
     if (event.which === 2) {
         const el = event.target;
         const isBlock = el.closest('.cat-card__container');
-
         if (isBlock) { isBlock.classList.toggle('cat-card__container--disable'); }
     }
-
 }
 
 
